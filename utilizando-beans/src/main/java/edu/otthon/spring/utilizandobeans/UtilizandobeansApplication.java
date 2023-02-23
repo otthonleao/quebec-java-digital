2c7e23782d70dc797cc3393173fab55259d1dca0
Otthon Leão <otthonleao@hotmail.com>
2023-02-23 05:30:30 -0400

    build: primeiro commit para iniciar as aulas sobre Beans no Spring Framework

---
+++ utilizando-beans/src/main/java/edu/otthon/spring/utilizandobeans/UtilizandobeansApplication.java
@@ -0,0 +1,13 @@
+package edu.otthon.spring.utilizandobeans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilizandobeansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilizandobeansApplication.class, args);
+	}
+
+}
---
+++ utilizando-beans/src/test/java/edu/otthon/spring/utilizandobeans/UtilizandobeansApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.otthon.spring.utilizandobeans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilizandobeansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
